e926d62d1b15b3d09cc40c42b30b7e961a989abd
letteldo <letteldo@mail.uni-mannheim.de>
2023-02-15 16:25:38 +0100

    Start new implementation for game logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,3 @@
-import game.Game;
-
 public class App {
-  public static void main(String[] args) {
-    Game.start(Integer.valueOf(args[0]));
-  }
+  public static void main(String[] args) {}
 }
--- src/main/java/game/PenguinColor.java
+++
@@ -1,5 +0,0 @@
-package game;
-
-public enum PenguinColor {
-    BLUE, RED, GREEN, YELLOW;
-}
